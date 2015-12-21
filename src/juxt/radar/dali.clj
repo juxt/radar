c10e5e773e7204ca66a11b4d3fa4585e734311fa
Jon Pither <jon.pither@gmail.com>
2015-12-21 13:01:19 +0000

    simply addding stack

--- src/juxt/radar/dali.clj
+++ src/juxt/radar/dali.clj
@@ -20,13 +20,12 @@
      [:line {:stroke [10 5]} [0 (/ height 2)] [width (/ height 2)]] ;; Horizonal line
      [:line {:stroke [10 5]} [(/ width 2) 0] [(/ width 2) height]] ;; Vertical line
      (for [{:keys [radius label]} (:arcs data)]
-       [:dali/stack {:direction :up :position [(/ width 2) (/ width 2)];; :anchor :left
-                     }
+       [:dali/stack {}
         [:circle
          {:stroke :indigo :stroke-width 1 :fill :none}
-         [(/ width 2) (/ width 2)] radius]
-        #_[:text
-         {:font-family "Arial" :font-size 20} label]])]))
+         [(/ width 2) (/ width 2)] radius]]
+       #_[:text
+          {:font-family "Arial" :font-size 20} label])]))
 
 (defn foo []
   (io/render-svg (radar radar-cfg) "foo.svg"))
